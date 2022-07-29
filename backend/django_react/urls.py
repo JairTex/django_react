05bd1de05f9c479725c55a41d889cd5f9871360f
JairTex <steixeirajair@gmail.com>
2022-07-29 00:20:26 -0300

    Correções nos arquivos urls.py do APP e do Projeto.

--- backend/django_react/urls.py
+++ backend/django_react/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('customers.urls')),
 ]
 
