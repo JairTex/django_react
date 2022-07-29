3914a574823160304762064999b2427c8f0b0272
JairTex <steixeirajair@gmail.com>
2022-07-29 00:20:03 -0300

    Edições nos arquivos urls.py do APP e do Projeto.

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -5,5 +5,5 @@ app_name = 'customers'
 
 urlpatterns = [
     path('customers/', views.CustomerListAPIView.as_view(),
-    name = 'customer-list')
+    name = 'customer-list'),
 ]
--- backend/django_react/urls.py
+++ backend/django_react/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
+    path('api/', include('customers.urls')),
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
